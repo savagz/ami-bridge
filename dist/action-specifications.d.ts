650d6bb4db6b7c313a4d75e810a586e1d5eff93f
Samuel Vargas <samuelvg@192.168.1.8>
2026-02-19 12:25:02 -0500

    Updated Action List

--- dist/action-specifications.d.ts
+++ dist/action-specifications.d.ts
@@ -1,7 +1,22 @@
 declare const _default: ({
     name: string;
     params: string[];
+    optional?: never;
     defaults?: never;
+} | {
+    name: string;
+    params: string[];
+    optional: string[];
+    defaults?: never;
+} | {
+    name: string;
+    params: string[];
+    defaults: {
+        LoadType: string;
+        format?: never;
+        mix?: never;
+        Paused?: never;
+    };
     optional?: never;
 } | {
     name: string;
@@ -16,33 +31,18 @@ declare const _default: ({
 } | {
     name: string;
     params: string[];
-    defaults: {
-        LoadType: string;
-        format?: never;
-        mix?: never;
-        Paused?: never;
-    };
-    optional?: never;
-} | {
-    name: string;
-    params: string[];
     optional: string[];
     defaults: {
         Paused: boolean;
+        LoadType?: never;
         format?: never;
         mix?: never;
-        LoadType?: never;
     };
 } | {
     name: string;
-    params: string[];
-    optional: string[];
-    defaults?: never;
-} | {
-    name: string;
     params?: never;
-    defaults?: never;
     optional?: never;
+    defaults?: never;
 })[];
 export default _default;
 //# sourceMappingURL=action-specifications.d.ts.map
